f008df27ed4ef3cf156f1e87140d0f9d0e00e21e
PonyPaucity <ivanang109@gmail.com>
2022-01-27 13:37:55 +0800

    Added score count for RPS game.
    Learned that condition 2 for 'for loop' must return false for loop to end

--- RPS.js
+++ RPS.js
@@ -19,22 +19,28 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === 'Rock') {
             return 'A Draw! Rock draws with Rock.'
         } else if (computerSelection === 'Paper') {
+            computerScore++;
             return 'You Lose! Paper beats Rock.'
         } else if (computerSelection === 'Scissors') {
+            playerScore++;
             return 'You Win! Rock beats Scissors.'
         }
     } else if (playerSelection === 'Paper') {
         if (computerSelection === 'Rock') {
+            playerScore++;
             return 'You Win! Paper beats Rock.'
         } else if (computerSelection === 'Paper') {
             return 'A Draw! Paper draws with Paper.'
         } else if (computerSelection === 'Scissors') {
+            computerScore++;
             return 'You Lose! Scissors beats Paper.'
         }
     } else if (playerSelection === 'Scissors') {
         if (computerSelection === 'Rock') {
+            computerScore++;
             return 'You Lose! Rock beats Scissors.'
         } else if (computerSelection === 'Paper') {
+            playerScore++;
             return 'You Win! Scissors beats Paper.'
         } else if (computerSelection === 'Scissors') {
             return 'A Draw! Scissors draws with Scissors.'
@@ -43,7 +49,7 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; playerScore < 5 && computerScore < 5; i++) {
         playerSelection = window.prompt("RPS");
         let computerSelection = computerPlay();
         console.log(playRound(playerSelection, computerSelection))
